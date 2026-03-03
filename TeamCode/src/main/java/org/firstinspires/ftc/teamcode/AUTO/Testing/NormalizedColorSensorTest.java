94d3ea903e13846cb52aa862b69d564992682001
Oleg Ryzhov <oleg.ryzhov@proton.me>
2026-03-03 15:12:28 +0000

    Fixed NormalizedColorSensorTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/Testing/NormalizedColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/Testing/NormalizedColorSensorTest.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.AUTO.Testing;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
@@ -17,41 +16,36 @@ public class NormalizedColorSensorTest extends LinearOpMode {
         String ball2;
         String ball3;
 
-        // Map the colors sensors on hardware
-        NormalizedColorSensor colorSensor = hardwareMap.get(NormalizedColorSensor.class, "color_left");
-        NormalizedColorSensor colorSensor2 = hardwareMap.get(NormalizedColorSensor.class, "color_mid");
-        NormalizedColorSensor colorSensor3 = hardwareMap.get(NormalizedColorSensor.class, "color_right");
+        // Map the color sensors on hardware
+        NormalizedColorSensor colorSensor = hardwareMap.get(NormalizedColorSensor.class, "ball_color_left");
+        NormalizedColorSensor colorSensor2 = hardwareMap.get(NormalizedColorSensor.class, "ball_color_mid");
+        NormalizedColorSensor colorSensor3 = hardwareMap.get(NormalizedColorSensor.class, "ball_color_right");
 
         waitForStart();
 
         while (opModeIsActive()) {
-            // Normalize the colors
+
+            // Read normalized colors
             NormalizedRGBA colors = colorSensor.getNormalizedColors();
             NormalizedRGBA colors2 = colorSensor2.getNormalizedColors();
             NormalizedRGBA colors3 = colorSensor3.getNormalizedColors();
 
-            // Ball 1
-            if (colors.alpha > colors.blue && colors.alpha > colors.green) {
-                ball1 = "No Color";
-            } else if (colors.blue > colors.green) {
+            // ---------------- Ball 1 ----------------
+            if (colors.blue > colors.green) { // Purple detection (works for you)
                 ball1 = "Purple";
-            } else {
+            } else { // anything else with enough brightness is green
                 ball1 = "Green";
             }
 
-            // Ball 2
-            if (colors2.alpha > colors2.blue && colors2.alpha > colors2.green) {
-                ball2 = "No Color";
-            } else if (colors2.blue > colors2.green) {
+            // ---------------- Ball 2 ----------------
+            if (colors2.blue > colors2.green) {
                 ball2 = "Purple";
             } else {
                 ball2 = "Green";
             }
 
-            // Ball 3
-            if (colors3.alpha > colors3.blue && colors3.alpha > colors3.green) {
-                ball3 = "No Color";
-            } else if (colors3.blue > colors3.green) {
+            // ---------------- Ball 3 ----------------
+            if (colors3.blue > colors3.green) {
                 ball3 = "Purple";
             } else {
                 ball3 = "Green";

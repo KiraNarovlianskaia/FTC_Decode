d4dbd2080ba8fcee0c8243661accbfe04180bf06
Oleg Ryzhov <oleg.ryzhov@proton.me>
2026-03-19 15:07:34 +0000

    Added 2 test programs to test systems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/MoveTest.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@Disabled
+@Autonomous (name="Move Test")
+public class MoveTest extends LinearOpMode {
+
+
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightFront;
+    DcMotor rightBack;
+    DcMotor intake;
+    DcMotor shooter;
+    static final double PI = 3.14159265;
+    static final double WHEEL_DIAMETER = 10.4;
+    static final double PULSES = 537.7;
+    static final double PULSES_PER_CM = PULSES / (PI * WHEEL_DIAMETER);
+    private IMU imu = null;
+
+
+    public void runOpMode() {
+
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        resetEncoders();
+
+
+        waitForStart();
+
+        moveForward(0.3, 80);
+        sleep(250);
+        moveForward(-0.3, 80);
+        sleep(1000);
+        moveSide(0.3, 80);
+        sleep(250);
+        moveSide(0.3, 80);
+
+    }
+    public void moveForward(double speed, double distance) {
+
+        resetEncoders();
+
+        leftFront.setPower(speed);
+        leftBack.setPower(speed);
+        rightFront.setPower(speed);
+        rightBack.setPower(speed);
+
+        while (opModeIsActive() && Math.abs(leftFront.getCurrentPosition()) < PULSES_PER_CM * distance) ;
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep(500);
+    }
+
+    public void moveSide(double speed, double distance) {
+
+        resetEncoders();
+
+        leftFront.setPower(speed);
+        leftBack.setPower(-speed);
+        rightFront.setPower(-speed);
+        rightBack.setPower(speed);
+
+        while (opModeIsActive() && Math.abs(leftFront.getCurrentPosition()) < PULSES_PER_CM * distance) ;
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep(500);
+    }
+
+    public void resetEncoders() {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ServoTestContinuous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ServoTestContinuous.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.AUTO.Testing;
+package org.firstinspires.ftc.teamcode.Testing;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;

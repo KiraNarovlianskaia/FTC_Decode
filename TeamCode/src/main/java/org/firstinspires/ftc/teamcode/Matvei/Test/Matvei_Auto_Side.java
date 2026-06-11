594ab8716e0645f8f902769506d60fc92c6d5b41
Matvei20132013 <matvei.riabov25@lumio.school>
2026-06-11 16:21:34 +0300

    -

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Matvei/Test/Matvei_Auto_Side.java
@@ -0,0 +1,166 @@
+package org.firstinspires.ftc.teamcode.Matvei.Test;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+@Autonomous(name="Matvei Leave Red")
+public class Matvei_Auto_Side extends LinearOpMode {
+    private DcMotor leftFront;
+    private DcMotor leftBack;
+    private DcMotor rightFront;
+    private DcMotor rightBack;
+
+    private IMU imu;
+
+    
+    static final double sidedistance = -25;
+    static  final double PI  = 3.1415;
+
+    static  final double DIAMETER  = 9.6;
+
+    static  final double PULSE =537.7;
+    static  final double PULSE_PER_CM =PULSE/(PI * DIAMETER);
+
+    public void runOpMode() {
+
+        RevHubOrientationOnRobot.LogoFacingDirection LogoDirection =
+                RevHubOrientationOnRobot.LogoFacingDirection.UP;
+
+        RevHubOrientationOnRobot.UsbFacingDirection UsbDirection =
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
+
+        RevHubOrientationOnRobot orientationOnRobot = new
+                RevHubOrientationOnRobot(LogoDirection, UsbDirection);
+
+
+        imu = hardwareMap.get(IMU.class,"imu");
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+
+        leftFront = hardwareMap.get(DcMotor.class, "left_front");
+        leftBack = hardwareMap.get(DcMotor.class, "left_back");
+        rightFront = hardwareMap.get(DcMotor.class, "right_front");
+        rightBack = hardwareMap.get(DcMotor.class, "right_back");
+
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        imu.resetYaw();
+        driveForward(0.1, 40);
+        sleep(10000);
+        leftFront.setPower(0.1);
+        leftBack.setPower(0.1);
+        rightFront.setPower(0.1);
+        rightBack.setPower(0.1);
+        sleep(5000);
+
+    }
+
+    public void rotate  (double rotateSpeed, double angle)
+    {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opModeIsActive() && Math.abs(getHeading()) < angle)
+        {
+            leftFront.setPower(-rotateSpeed);
+            leftBack.setPower(-rotateSpeed);
+            rightFront.setPower(rotateSpeed);
+            rightBack.setPower(rotateSpeed);
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep( 400);
+    }
+    public void driveForward (double forwardSpeed, double distance)
+    {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opModeIsActive() && Math.abs(leftFront.getCurrentPosition()) < PULSE_PER_CM * distance)
+        {
+            leftFront.setPower(forwardSpeed);
+            leftBack.setPower(forwardSpeed);
+            rightFront.setPower(forwardSpeed);
+            rightBack.setPower(forwardSpeed);
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep( 100);
+    }
+    public void sideLeft (double sideSpeed, double distance)
+    {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opModeIsActive() && leftFront.getCurrentPosition() > sidedistance*distance)
+        {
+            leftFront.setPower(-sideSpeed);
+            leftBack.setPower(sideSpeed);
+            rightFront.setPower(sideSpeed);
+            rightBack.setPower(-sideSpeed);
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep( 100);
+    }
+
+    public void sideRight (double sideSpeed, double distance)
+    {
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        while (opModeIsActive() && leftFront.getCurrentPosition() < -sidedistance*distance)
+        {
+            leftFront.setPower(sideSpeed);
+            leftBack.setPower(-sideSpeed);
+            rightFront.setPower(-sideSpeed);
+            rightBack.setPower(sideSpeed);
+        }
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        sleep( 100);
+    }
+    public double getHeading(){
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
+    }
+}
